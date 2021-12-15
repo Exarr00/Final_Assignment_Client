ccf3694251088728bbd1706e87a7d437f39e41cc
Andy N <Andy.Ni87@myhunter.cuny.edu>
2021-12-15 13:44:39 -0500

    put navbar and style in seperate file

---
+++ src/components/views/styles/navhome.js
@@ -0,0 +1,37 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+      textAlign: 'left',
+      fontType: 'bold',
+      fontFamily: 'Courier, sans-serif', 
+      fontSize: '35px', 
+      color: '#CDDC39'
+    },
+    appBar:{
+      backgroundColor: '#11153e',
+      shadows: ['none'],
+    },
+    greeting:{
+      display: 'flex',
+      flexDirection:'column',
+      justifyContent: 'center',
+      alignItems:'center',
+      width: "50%",
+      margin: "auto",
+      marginTop:"50px",
+    },
+    links:{
+      textDecoration: 'none',
+    },
+    pic: {
+      height:"50vh"
+    }
+  
+  }));
+
+export {useStyles}
